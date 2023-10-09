4e87932ae669ece5a07220831895589977003f3f
lukasz-reactedupl <lukasz.magiera.nc@gmail.com>
2023-10-09 22:28:30 +0200

    Initial bulma setup added

---
+++ lab2/src/App.js
@@ -0,0 +1,35 @@
+const CharacterListItem = () => {
+    return (
+        <div className="box">
+            <div className="media">
+                <div className="media-left">
+                    <figure className="image is-64x64">
+                        <img
+                            src="https://gravatar.com/avatar/62e6ef0bd980dc031d4d5ce83cfe4ddf?s=400&d=robohash&r=x"
+                            alt={`Avatar of Mickey Mouse`}
+                            className="is-rounded"
+                        />
+                    </figure>
+                </div>
+                <div className="media-content">
+                    <p className="title is-5">Mickey Mouse</p>
+                    <p className="subtitle is-6">mickey@example.com</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div className="App">
+            <div className="columns is-centered">
+                <div className="column is-4">
+                    <CharacterListItem />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
